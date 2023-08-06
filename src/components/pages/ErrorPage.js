7458bd8a710dd02bed83cab7af9ece4613254735
SalimSK15 <salimdu15@outlook.fr>
2023-08-05 20:14:23 -0400

    ✨ F02: PAGES > add redirect link

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -5,7 +5,7 @@ function ErrorPage() {
   return (
     <div>
       <h1>Erreurs 404 ("Page introuvable")</h1>
-      <Link >Retourner vers la page d'accueil</Link>
+      <Link to="/LoginPage">Retourner vers la page d'accueil</Link>
     </div>
   )
 }
