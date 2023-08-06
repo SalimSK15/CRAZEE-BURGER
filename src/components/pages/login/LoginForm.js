8567bad019220f03419329581eff5282516030ac
SalimSK15 <salimdu15@outlook.fr>
2023-08-05 20:07:59 -0400

    ✨ F02: PAGES > Add redirect

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
+import { useNavigate   } from 'react-router-dom';
 
 export default function LoginForm() {
     //state
    const [prenom, setPrenom] = useState("");
-   
+   const navigate   = useNavigate();
+
    // comporetement
    const handleSubmit = (e) => {
      e.preventDefault();
-     alert(`Bonjour ${prenom}`)
-     setPrenom("")
-
+     if(prenom.trim() !== ""){
+      navigate( "/OrderPage",{state: {prenom}})
+     }
    }
    const handleChange = (e) => {
      setPrenom(e.target.value); 
@@ -21,8 +23,8 @@ export default function LoginForm() {
       <h1>Bienvenue chez nous !</h1>
       <br />
       <h2>Connectez-vous</h2>
-      <input onChange={handleChange} value={prenom } type="text" placeholder="Entre votre prenom ..." required/>
-      <button>Accedez a votre espace</button>
+      <input onChange={handleChange} value={prenom } type="text" placeholder="Entrez votre prénom ..." required/>
+      <button>Accédez à votre espace</button>
     </form>
   )
 }
