7185c9fc903f278989bedd84c610b86b00b1c966
SalimSK15 <salimdu15@outlook.fr>
2023-08-05 20:04:03 -0400

    ✨ F02 : PAGE > Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './components/pages/login/LoginPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import OrderPage from './components/pages/OrderPage';
+import ErrorPage from './components/pages/ErrorPage';
 
 function App() {
   return (
-    <LoginPage />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/LoginPage" element={<LoginPage />}/>
+          <Route path="/OrderPage" element={ <OrderPage />}/>
+          <Route path="/*" element={ <ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+    
   );
 }
 
