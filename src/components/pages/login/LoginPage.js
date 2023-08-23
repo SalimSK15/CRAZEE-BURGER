571e3e50dc3ff3cad1273d63c10b8bec28a8951e
SalimSK15 <salimdu15@outlook.fr>
2023-08-22 21:06:44 -0400

    💄 F03: LOGIN> LoginPage > ajout style & composant Logo

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,13 +1,38 @@
+import { styled } from "styled-components";
 import LoginForm from "./LoginForm";
+import Logo from "../../reusable-ui/Logo";
 
 export default function LoginPage() {
 
   // affichage (render)
-
-  
   return (
-    <div>
+    <LoginPageStyled>
+      <Logo />
       <LoginForm />
-    </div>
+    </LoginPageStyled>
   )
 }
+const LoginPageStyled = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  &::before{
+    content: "";
+    background: url("/images/F03-burger-background.jpg") 
+    rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    inset: 0px;
+    // top: 0;
+    // left: 0;
+    // right: 0;
+    // bottom: 0;
+    z-index: -1;
+  }
+`
