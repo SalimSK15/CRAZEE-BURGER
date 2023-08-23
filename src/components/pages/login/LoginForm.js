847f33def635594db217272ce2768d4badb03ff5
SalimSK15 <salimdu15@outlook.fr>
2023-08-22 21:08:02 -0400

    ✅ F03: LOGIN> LoginForm > ajout style et TextInput

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import { useNavigate   } from 'react-router-dom';
+import { styled } from 'styled-components';
+import { BsPersonCircle } from 'react-icons/bs';
+import TextInput from '../../reusable-ui/TextInput';
+import PrimaryButton from '../../reusable-ui/PrimaryButton';
+import { IoChevronForward} from "react-icons/io5";
+import { theme } from '../../../theme';
 
 export default function LoginForm() {
     //state
    const [prenom, setPrenom] = useState("");
    const navigate   = useNavigate();
 
-   // comporetement
+   // comporetement 
    const handleSubmit = (e) => {
      e.preventDefault();
      navigate( `/order/${prenom}`)
@@ -17,12 +23,56 @@ export default function LoginForm() {
   // affichage (render)
 
   return (
-    <form action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <input onChange={handleChange} value={prenom } type="text" placeholder="Entrez votre prénom ..." required/>
-      <button>Accédez à votre espace</button>
-    </form>
+    <LoginFormStyled action="submit" onSubmit={handleSubmit}>
+      <div>
+        <h1>Bienvenue chez nous !</h1>
+        <hr />
+        <h2>Connectez-vous</h2>
+      </div>
+      <TextInput 
+      value={prenom} 
+      onChange={handleChange}
+      placeholder={"Entrer votre prénom"} 
+      Icon={<BsPersonCircle className="icon" />}
+      required
+      />
+      <PrimaryButton 
+        label={"Accéder à votre espace"} 
+        Icon={ <IoChevronForward 
+          className="icon"/>}
+      />
+    </LoginFormStyled>
   )
 }
+const LoginFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0 auto;
+  padding: 40px ${theme.spacing.lg};
+  border-radius: ${theme.borderRadius.round};
+  font-family: "Amatic SC", cursive;
+
+  hr{
+    border: 1.5px solid ${theme.colors.loginLine};
+    margin-bottom: ${theme.gridUnit * 5}px;
+  }
+  h1{
+    color: white;
+    font-size: ${theme.fonts.size.P5};
+  }
+  h2{
+    margin: 20px 10px 10p;
+    color: ${theme.colors.white};
+    font-size: ${theme.fonts.size.P4};
+  }
+  .icon{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size:${theme.fonts.size.P0};
+    margin-left: 10px;
+  }
+`;
